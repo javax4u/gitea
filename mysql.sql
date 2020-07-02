8160504344533a272bb3908b4f468c9fea686b51
rupesh kumar <javax4u@gmail.com>
2020-07-02 23:00:14 +0530

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,9 +1,7 @@
-sudo mysql
-CREATE USER 'gitea'@'localhost' IDENTIFIED BY 'gitea';                                                                                                                                                                                                
-GRANT ALL PRIVILEGES ON * . * TO 'gitea'@'localhost';                                                                                                                                                                                                
-FLUSH PRIVILEGES;                                                                                                                                                                                               
-SHOW GRANTS FOR 'gitea'@'localhost';                                                                                                                                                                                                                                                                                                                                                                                             
-EXIT
-mysql -u gitea -pgitea
+DROP USER  gitea@localhost;
+CREATE USER 'gitea'@'localhost' IDENTIFIED BY 'gitea';
+GRANT ALL PRIVILEGES ON * . * TO 'gitea'@'localhost';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'gitea'@'localhost';
 CREATE DATABASE gitea;
 EXIT
