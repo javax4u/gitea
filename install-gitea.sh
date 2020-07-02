0503d490c885b9a3412ed91aa55bb53a8678abe1
rupesh kumar <javax4u@gmail.com>
2020-07-02 22:38:20 +0530

    Update install-gitea.sh

--- install-gitea.sh
+++ install-gitea.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-sudo mysql
-CREATE USER 'gitea'@'localhost' IDENTIFIED BY 'gitea';                                                                                                                                                                                                
-GRANT ALL PRIVILEGES ON * . * TO 'gitea'@'localhost';                                                                                                                                                                                                
-FLUSH PRIVILEGES;                                                                                                                                                                                               
-SHOW GRANTS FOR 'gitea'@'localhost';                                                                                                                                                                                                                                                                                                                                                                                             
-EXIT
-mysql -u gitea -pgitea
-CREATE DATABASE gitea;
-EXIT
-
 
 adduser \
    --system \
