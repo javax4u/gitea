7be0dd6b985d58c55cd67557c6a36a27073e78c0
rupesh kumar <javax4u@gmail.com>
2020-07-02 21:52:44 +0530

    Update install-gitea.sh

--- install-gitea.sh
+++ install-gitea.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#1. copy files into desired folder
-
 adduser \
    --system \
    --shell /bin/bash \
@@ -9,16 +7,14 @@ adduser \
    --disabled-password \
    --home /home/git \
    git
-cd /home/gitea
+cd /home/git
 #download gitea binary
 wget -O gitea https://dl.gitea.io/gitea/1.12.1/gitea-1.12.1-linux-amd64
-wget <input gitea.service download link from git here>
 #make it executable
 chmod +x gitea
 #verify signature
-gpg --keyserver keys.openpgp.org --recv 7C9E68152594688862D62AF62D9AE806EC1592E2
-gpg --verify gitea-1.12.1-linux-amd64.asc gitea-1.12.1-linux-amd64
-
+#gpg --keyserver keys.openpgp.org --recv 7C9E68152594688862D62AF62D9AE806EC1592E2
+#gpg --verify gitea-1.12.1-linux-amd64.asc gitea-1.12.1-linux-amd64
 #make directory 
 mkdir -p /var/lib/gitea/{custom,data,log}
 #gitea will generate ini file here for first time configuration
@@ -29,12 +25,7 @@ export GITEA_WORK_DIR=/var/lib/gitea/
 #Copy Gitea binary to global location
 cp gitea /usr/local/bin/gitea
 chmod 777 /usr/local/bin/gitea
-
-chown -R root:root /var/lib/gitea/
-sh uninstall-gitea.sh
-sh install-gitea.sh
-service gitea status
-
+#chown -R root:root /var/lib/gitea/
 cp gitea.service /etc/systemd/system
 chmod -R 777 /etc/systemd/system/gitea.service
 cd /usr/local/bin
