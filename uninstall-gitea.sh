292be2b1a29afbfc4c400b188828c6208efd56ba
rupesh kumar <javax4u@gmail.com>
2020-07-02 22:24:17 +0530

    Update uninstall-gitea.sh

--- uninstall-gitea.sh
+++ uninstall-gitea.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
 systemctl stop gitea
 systemctl disable gitea
+userdel git
+rm -R /home/git
+rm -R /var/lib/gitea/
+rm -R /etc/gitea
+rm /usr/local/bin/gitea
 rm /etc/systemd/system/gitea.service
 #rm -R /opt/vdoxx/RFIDListener
 systemctl daemon-reload
 systemctl reset-failed
+
